e9ff33890cfe7aa1de481eab93968daa54e265cf
ame5r <ameer.gneem@gmail.com>
2021-03-07 11:37:30 +0200

    Back button removed

--- lib/screens/MyHomePage.dart
+++ lib/screens/MyHomePage.dart
@@ -39,6 +39,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
+        leading: new Container(),
         title: new Text(
           sAppName,
           textDirection: TextDirection.rtl,
